e3b80da0055f91a9a9647ac1b10928000948353b
qborisb <boris@qdentity.nl>
2022-11-16 10:38:11 +0100

    Remove default exception handler

--- lib/filter.ex
+++ lib/filter.ex
@@ -7,13 +7,11 @@ defmodule SentryEventFilter.Filter do
     quote do
       @behaviour Sentry.EventFilter
 
-      def exclude_exception(%Phoenix.Router.NoRouteError{}, _source), do: true
+      def exclude_exception?(%Phoenix.Router.NoRouteError{}, _source), do: true
 
-      def exclude_exception(%Phoenix.NotAcceptableError{}, _source), do: true
+      def exclude_exception?(%Phoenix.NotAcceptableError{}, _source), do: true
 
       def exclude_exception?(%Plug.CSRFProtection.InvalidCSRFTokenError{}, :plug), do: true
-
-      def exclude_exception?(_exception, _source), do: false
     end
   end
 end
