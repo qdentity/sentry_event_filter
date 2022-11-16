d39ade799621c0f753d9859b57a55461bb3ec83a
qborisb <boris@qdentity.nl>
2022-11-16 10:44:54 +0100

    Revert filter changes

--- lib/filter.ex
+++ lib/filter.ex
@@ -2,16 +2,13 @@ defmodule SentryEventFilter.Filter do
   @moduledoc """
   This module defines exceptions to be excluded from sentry
   """
+  @behaviour Sentry.EventFilter
 
-  defmacro __using__(_opts) do
-    quote do
-      @behaviour Sentry.EventFilter
+  def exclude_exception?(%Phoenix.Router.NoRouteError{}, _source), do: true
 
-      def exclude_exception?(%Phoenix.Router.NoRouteError{}, _source), do: true
+  def exclude_exception?(%Phoenix.NotAcceptableError{}, _source), do: true
 
-      def exclude_exception?(%Phoenix.NotAcceptableError{}, _source), do: true
+  def exclude_exception?(%Plug.CSRFProtection.InvalidCSRFTokenError{}, :plug), do: true
 
-      def exclude_exception?(%Plug.CSRFProtection.InvalidCSRFTokenError{}, :plug), do: true
-    end
-  end
+  def exclude_exception?(_exception, _source), do: false
 end
